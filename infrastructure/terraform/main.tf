3e91ed2f60368407f4bd6310bfbba353c3bc8973
David Eriksson <davideriksson@swedenmail.com>
2017-05-22 23:18:16 +0200

    Loadbalancer with simple server and autoscaling

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -2,7 +2,123 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = 8080
+}
+
+# Simple server instance
 resource "aws_instance" "example" {
   ami = "ami-2d39803a"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+
+  tags {
+    Name = "terraform-example"
+  }
+}
+
+# AWS Security group for aws_launch_configuration
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Launch configuration for autoscaling
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-2d39803a"
+  instance_type = "t2.micro"
+  security_groups = ["${aws_security_group.instance.id}"]
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Configures available aws zones
+data "aws_availability_zones" "all" {}
+
+# Defines scaling
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+  min_size = 2
+  max_size = 3
+
+  load_balancers = ["${aws_elb.example.name}"]
+  health_check_type = "ELB"
+
+  tag {
+    key = "Name"
+    propagate_at_launch = true
+    value = "terraform-asg-example"
+  }
+}
+
+# Configurations for LB
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Proxy and healthchecks for LB
+resource "aws_elb" "example" {
+  name = "terraform-asg-example"
+  security_groups = ["${aws_security_group.elb.id}"]
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:${var.server_port}/"
+  }
+
+  listener {
+    lb_port = 80
+    lb_protocol = "http"
+    instance_port = "${var.server_port}"
+    instance_protocol = "http"
+  }
+}
+
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
+}
+
+output "elb_dns_name" {
+  value = "${aws_elb.example.dns_name}"
 }
